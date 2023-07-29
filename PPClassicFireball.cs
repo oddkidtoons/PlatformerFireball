16f4acc7bfb30a6750f01ca7c75762d80e1391e3
oddkidtoons <oddkidtoons@gmail.com>
2023-07-29 11:32:16 -0400

    added Platformer Project Default Enemy version of the fireball

---
+++ PPClassicFireball.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+
+
+
+public class PPClassicFireball : MonoBehaviour
+	{
+	
+		
+		public Fireball_Input playerControls;
+	private InputAction fire;
+
+	public GameObject fireball;
+		public Transform fireball_spawn_loc;
+		public Vector3 fireballVel;
+
+		private Animator anim;
+		private Animator camAnim;
+
+	private void Awake()
+	{
+		playerControls = new Fireball_Input();
+	}
+
+	private void OnEnable()
+	{
+		fire = playerControls.Player.Fire;
+		fire.Enable();
+		fire.performed += shootFireBall;
+
+			}
+
+	private void OnDisable()
+	{
+		fire.Disable();
+		
+	}
+
+	private void shootFireBall(InputAction.CallbackContext Context) 
+	
+	{
+				StartCoroutine(Shoot_Fireball());
+		
+		}
+
+		IEnumerator Shoot_Fireball()
+		{
+			
+				yield return new WaitForSeconds(0.1f);
+				GameObject Clone = Instantiate(fireball, fireball_spawn_loc.position, fireball.transform.rotation);
+				Clone.GetComponent<PPFireball>().enabled = true;
+				Clone.transform.GetChild(0).gameObject.SetActive(true);
+				Clone.GetComponent<Rigidbody>().velocity = transform.TransformDirection(fireballVel.x, fireballVel.y, fireballVel.z);//initial speed
+		
+
+		}
+
+
+	}
---
+++ PPFireball.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+using PLAYERTWO.PlatformerProject;
+
+
+
+    public class PPFireball : MonoBehaviour
+    {
+        public string EnemyTag;
+        private Rigidbody rb;
+
+        private Vector3 velocity;
+
+        public float TimeToDestroy = 1f;
+
+        public Transform FireballObject;
+        public GameObject FireChild;
+
+        public int damageAmount = 1;
+
+        float counter;
+        private void Awake()
+        {
+            StartCoroutine(timedDeath());
+        }
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+            rb = GetComponent<Rigidbody>();
+            velocity = rb.velocity;
+            //Assigns the transform of the first child of the Game Object this script is attached to.
+            // FireballObject = FireballObject.gameObject.transform.GetChild(0);
+            //Assigns the first child of the first child of the Game Object this script is attached to.
+            FireChild = FireballObject.gameObject.transform.GetChild(0).gameObject;
+            FireChild.SetActive(true);
+
+
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            Vector3 gravity = 120 * Vector3.down; //cant simulate fireball bounces with normal realworld gravity, so i ad a downwards force that i can change from script, simulating gravity for fireball only
+            rb.AddForce(gravity, ForceMode.Acceleration);
+
+            if (rb.velocity.y < velocity.y) //to avoid arcs formed when mario initially shoots fireball
+            {
+                rb.velocity = velocity;
+            }
+
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent<Enemy>(out var enemy))
+            { 
+                enemy.ApplyDamage(damageAmount);
+            Invoke("Explode", delayExplode);
+            Destroy(this.gameObject);
+        }
+        }
+
+        void OnCollisionEnter(Collision col)
+        {
+
+            if (col.contacts[0].normal.y > 0.4 && col.contacts[0].normal.y < 1.6)
+            {
+                rb.velocity = new Vector3(velocity.x, -velocity.y, velocity.z);
+            }
+
+            //reflect
+            if (col.contacts[0].normal.x > 0.3 || col.contacts[0].normal.z > 0.3f || col.contacts[0].normal.x < -0.3f || col.contacts[0].normal.z < -0.3f)
+            {
+                Vector3 oldVel = velocity;
+                oldVel = oldVel.normalized;
+                oldVel *= 1900 * Time.deltaTime;
+
+                Vector3 newvel = Vector3.Reflect(oldVel, col.contacts[0].normal);
+
+                velocity = new Vector3(newvel.x, oldVel.y, newvel.z);
+                //rb.velocity = rb.velocity;
+
+
+               
+
+            }
+
+        }
+
+        public GameObject effect;
+        private GameObject effectClone;
+        public float delayExplode;
+
+
+        void Explode()
+        {
+            effectClone = effect;
+            Instantiate(effectClone, gameObject.transform.position, gameObject.transform.rotation);
+            //Destroy(effectClone);
+        }
+
+
+        public IEnumerator Destroy()
+        {
+            transform.GetComponent<MeshRenderer>().enabled = false;
+            transform.GetComponent<SphereCollider>().enabled = false;
+            transform.GetComponent<ParticleSystem>().Stop();
+            GameObject Dissolve = transform.GetChild(0).gameObject;
+            GameObject Clone = Instantiate(Dissolve, transform.position, Dissolve.transform.rotation);
+            Clone.GetComponent<ParticleSystem>().Play();
+            Destroy(Clone, 3);
+            yield return new WaitForSeconds(1);
+            Destroy(gameObject);
+
+        }
+
+        IEnumerator timedDeath()
+        {
+            yield return new WaitForSeconds(TimeToDestroy);
+            Object.Destroy(this.gameObject);
+        }
+    } 
