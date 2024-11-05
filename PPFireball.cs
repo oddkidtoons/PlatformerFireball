cb6de923954020ac1ad9db35437d056347487c04
OddKid <102359624+oddkidtoons@users.noreply.github.com>
2024-11-04 20:07:22 -0500

    Separated Fireball and Projectile Scripts
    For the sake of simplicity, I am making two separate scripts for Projectiles and Fireballs.

--- PPFireball.cs
+++ PPFireball.cs
@@ -4,40 +4,84 @@ using PLAYERTWO.PlatformerProject;
 using UnityEngine.Events;
 
 namespace PLAYERTWO.PlatformerProject{
-
+[RequireComponent(typeof(AudioSource))]
     public class PPFireball : MonoBehaviour
     {
-        public GameObject impactParticle; // Effect spawned when projectile hits a collider
-        public GameObject projectileParticle; // Effect attached to the gameobject as child
-        public GameObject muzzleParticle; // Effect instantly spawned when gameobject is spawned
-        [Header("Adjust if not using Sphere Collider")]
-        public float colliderRadius = 1f;
-        [Range(0f, 1f)] // This is an offset that moves the impact effect slightly away from the point of impact to reduce clipping of the impact effect
-        public float collideOffset = 0.15f;
-
-         [Header("Attack Settings")]
+        
+AudioSource audioSource;
+      [Header("Attack Settings")]
 		public bool breakObjects;
 		public int breakableDamage = 1;
+
 		protected Collider m_collider;
-public LayerMask myLayerMask; //Select the layers that the fireball ignores. 
-public int enemyDamageAmount = 1; //PP Enemy Damage setting
+       
+
+        private Rigidbody rb;
+
+        private Vector3 velocity;
+
+        public float TimeToDestroy = 1f;
+
+        public Transform FireballParent;
+        public GameObject BulletVFX;
+        public AudioClip bulletAudio;
+        public GameObject MuzzleVFX;
+
+        public int enemyDamageAmount = 1;
+
+        float counter;
+
+          public GameObject ExplosionVFX;
+        public AudioClip explosionAudio;
+        private GameObject effectClone;
+        public float delayExplode;
+public UnityEvent DamageEvent;
+
+   
+
+        private void Awake()
+        {
+            StartCoroutine(timedDeath());
+        
+        }
 
 
-public UnityEvent onExplode; //Events that trigger when the projectile hits something
-
-
+        // Use this for initialization
         void Start()
         {
-             InitializeCollider();
-            projectileParticle = Instantiate(projectileParticle, transform.position, transform.rotation) as GameObject;
-            projectileParticle.transform.parent = transform;
-            if (muzzleParticle)
-            {
-                muzzleParticle = Instantiate(muzzleParticle, transform.position, transform.rotation) as GameObject;
-                Destroy(muzzleParticle, 1.5f); // 2nd parameter is lifetime of effect in seconds
+             audioSource = gameObject.GetComponent<AudioSource>();
+            if (bulletAudio != null){ 
+           
+            audioSource.PlayOneShot(bulletAudio);}
+
+            if (ExplosionVFX != null){ 
+            ExplosionVFX.SetActive(false);}
+
+                  if (MuzzleVFX != null){
+            MuzzleVFX.SetActive(true);
             }
+			
+            InitializeCollider();
+
+            rb = GetComponent<Rigidbody>();
+            velocity = rb.velocity;
+
+          
+            //Assigns the transform of the first child of the Game Object this script is attached to.
+            // FireballObject = FireballObject.gameObject.transform.GetChild(0);
+            //Assigns the first child of the first child of the Game Object this script is attached to.
+            if (BulletVFX != null){
+                
+            BulletVFX = FireballParent.gameObject.transform.GetChild(0).gameObject;
+          
+            BulletVFX.SetActive(true);
+            }
+
+
         }
-        protected virtual void InitializeCollider()
+
+
+		protected virtual void InitializeCollider()
 		{
 			m_collider = GetComponent<Collider>();
 			m_collider.isTrigger = true;
@@ -58,68 +102,39 @@ public UnityEvent onExplode; //Events that trigger when the projectile hits some
 			breakable.ApplyDamage(breakableDamage);
 
 		}
-		
+
+        // Update is called once per frame
         void FixedUpdate()
-        {	
-			if (GetComponent<Rigidbody>().velocity.magnitude != 0)
-			{
-			    transform.rotation = Quaternion.LookRotation(GetComponent<Rigidbody>().velocity); // Sets rotation to look at direction of movement
-			}
-			
-            RaycastHit hit;
-			
-            float radius; // Sets the radius of the collision detection
-            if (transform.GetComponent<SphereCollider>())
-                radius = transform.GetComponent<SphereCollider>().radius;
-            else
-                radius = colliderRadius;
+        {
+            Vector3 gravity = 120 * Vector3.down; //cant simulate fireball bounces with normal realworld gravity, so i ad a downwards force that i can change from script, simulating gravity for fireball only
+            rb.AddForce(gravity, ForceMode.Acceleration);
 
-            Vector3 direction = transform.GetComponent<Rigidbody>().velocity; // Gets the direction of the projectile, used for collision detection
-            if (transform.GetComponent<Rigidbody>().useGravity)
-                direction += Physics.gravity * Time.deltaTime; // Accounts for gravity if enabled
-            direction = direction.normalized;
-
-            float detectionDistance = transform.GetComponent<Rigidbody>().velocity.magnitude * Time.deltaTime; // Distance of collision detection for this frame
-
-            if (Physics.SphereCast(transform.position, radius, direction, out hit, detectionDistance)) // Checks if collision will happen
+            if (rb.velocity.y < velocity.y) //to avoid arcs formed when mario initially shoots fireball
             {
-                transform.position = hit.point + (hit.normal * collideOffset); // Move projectile to point of collision
-
-                GameObject impactP = Instantiate(impactParticle, transform.position, Quaternion.FromToRotation(Vector3.up, hit.normal)) as GameObject; // Spawns impact effect
-
-                ParticleSystem[] trails = GetComponentsInChildren<ParticleSystem>(); // Gets a list of particle systems, as we need to detach the trails
-                //Component at [0] is that of the parent i.e. this object (if there is any)
-                for (int i = 1; i < trails.Length; i++) // Loop to cycle through found particle systems
-                {
-                    ParticleSystem trail = trails[i];
-
-                    if (trail.gameObject.name.Contains("Trail"))
-                    {
-                        trail.transform.SetParent(null); // Detaches the trail from the projectile
-                        Destroy(trail.gameObject, 2f); // Removes the trail after seconds
-                    }
-                }
-onExplode?.Invoke();
-                Destroy(projectileParticle, 3f); // Removes particle effect after delay
-                Destroy(impactP, 3.5f); // Removes impact effect after delay
-                Destroy(gameObject); // Removes the projectile
+                rb.velocity = velocity;
             }
+
         }
-          protected virtual void HandleCustomCollision(Collider other) { }
+        
+        
+        protected virtual void HandleCustomCollision(Collider other) { }
 
       private void OnTriggerEnter(Collider other)
 {    
 
+      if (explosionAudio != null){ 
+        AudioSource.PlayClipAtPoint(explosionAudio, transform.position, 1f);}
 
     if (other.TryGetComponent<Enemy>(out var enemy))
     {
-
-        
         enemy.ApplyDamage(enemyDamageAmount, transform.position);
+if (ExplosionVFX != null){ 
+        Invoke("Explode", delayExplode);}
+   if (DamageEvent != null){
+                DamageEvent.Invoke();}
                   
-
+      
         HandleCustomCollision(other);
-  
     Destroy(this.gameObject);
         
       
@@ -128,6 +143,72 @@ onExplode?.Invoke();
     HandleCollision(other);
     
 }
-   
-    }
-}
+
+
+        void OnCollisionEnter(Collision col)
+        {
+            if (explosionAudio != null){ 
+         AudioSource.PlayClipAtPoint(explosionAudio, transform.position, 1f);}
+
+            if (col.contacts[0].normal.y > 0.4 && col.contacts[0].normal.y < 1.6)
+            {
+                rb.velocity = new Vector3(velocity.x, -velocity.y, velocity.z);
+            }
+
+            //reflect
+            if (col.contacts[0].normal.x > 0.3 || col.contacts[0].normal.z > 0.3f || col.contacts[0].normal.x < -0.3f || col.contacts[0].normal.z < -0.3f)
+            {
+                Vector3 oldVel = velocity;
+                oldVel = oldVel.normalized;
+                oldVel *= 1900 * Time.deltaTime;
+
+                Vector3 newvel = Vector3.Reflect(oldVel, col.contacts[0].normal);
+
+                velocity = new Vector3(newvel.x, oldVel.y, newvel.z);
+            
+           if (ExplosionVFX != null){
+                Invoke("Explode", delayExplode);}
+                
+ Destroy(this.gameObject);
+
+
+        }
+
+        }   
+
+
+        public void Explode()
+        {
+           
+            ExplosionVFX.SetActive(true);
+    
+            effectClone = ExplosionVFX;
+            GameObject Clone = Instantiate(effectClone, transform.position, Quaternion.FromToRotation(Vector3.up, transform.up)) as GameObject;
+           
+        Clone.GetComponent<ParticleSystem>().Play();
+      
+           Destroy(Clone);
+           
+        }
+
+
+        public IEnumerator Destroy()
+        {
+            transform.GetComponent<MeshRenderer>().enabled = false;
+            transform.GetComponent<SphereCollider>().enabled = false;
+            transform.GetComponent<ParticleSystem>().Stop();
+            GameObject Dissolve = transform.GetChild(0).gameObject;
+            GameObject Clone = Instantiate(Dissolve, transform.position, Quaternion.FromToRotation(Vector3.up, transform.up)) as GameObject;
+            Clone.GetComponent<ParticleSystem>().Play();
+            Destroy(Clone);
+            yield return new WaitForSeconds(1);
+            Destroy(gameObject);
+
+        }
+
+        IEnumerator timedDeath()
+        {
+            yield return new WaitForSeconds(TimeToDestroy);
+            Object.Destroy(this.gameObject);
+        }
+    } }
---
+++ PPProjectileTrigger.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+
+namespace PLAYERTWO.PlatformerProject{
+
+public class PPProjectileTrigger : MonoBehaviour
+	{
+	
+		
+		public OddKid_Input playerControls;
+	private InputAction fire;
+
+	public GameObject projectile;
+		public Transform projectile_spawn_loc;
+		public Vector3 projectileVel;
+
+		private Animator anim;
+		private Animator camAnim;
+
+	private void Awake()
+	{
+		playerControls = new OddKid_Input();
+	}
+
+	private void OnEnable()
+	{
+		fire = playerControls.Player.Fire;
+		fire.Enable();
+		fire.performed += shootFireBall;
+
+			}
+
+	private void OnDisable()
+	{
+		fire.Disable();
+		
+	}
+
+	private void shootFireBall(InputAction.CallbackContext Context) 
+	
+	{
+				StartCoroutine(Shoot_Projectile());
+		
+		}
+
+		IEnumerator Shoot_Projectile()
+		{
+			
+				yield return new WaitForSeconds(0.1f);
+				GameObject Clone = Instantiate(projectile, projectile_spawn_loc.position, projectile.transform.rotation);
+				Clone.GetComponent<PPProjectile>().enabled = true;
+				//Clone.transform.GetChild(0).gameObject.SetActive(true);
+				Clone.GetComponent<Rigidbody>().velocity = transform.TransformDirection(projectileVel.x, projectileVel.y, projectileVel.z);//initial speed
+		
+
+		}
+
+
+	}
+}
